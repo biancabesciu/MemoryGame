47911aac2301ca60e7e72e425a6d2c53c5ee4cf1
Bianca Besciu <bianca.besciu@gmail.com>
2017-09-11 13:55:17 +0200

    include some scss

--- scripts/main.babel.js
+++ scripts/main.babel.js
@@ -5,29 +5,3 @@
  // The goal of the game is to get all the tiles flipped face up (i.e., find all the matching image pairs) in the least number of tries. That means that lower number of tries are better scores.
  */
 
-// creating face-down tiles
-//define the Tile constructor function
-function Tile(x, y) {
-    this.x = x;
-    this.y = y;
-    this.width = 70;
-}
-
-//create tiles at appropiate x and y positions
-let tiles = [];
-const numCols = 5;
-const numRows = 4;
-
-for (let i = 0; i < numCols; i++) {
-    for (let j = 0; j < numRows; j++) {
-        tiles.push(new Tile(i * 78 + 10, j * 78 + 40));
-    }
-}
-
-//draw tiles face down
-Tile.prototype.drawFaceDown = function () {
-    fill(214, 247, 202);
-    strokeWeight(2);
-    rect(this.x, this.y, this.width, this.width, 10);
-};
-
